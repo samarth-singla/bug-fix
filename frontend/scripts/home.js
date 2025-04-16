30fde5f244975d40ea002740adf9cf884a05117b
samarth-singla <samysingla146@gmail.com>
2025-04-16 16:31:25 +0530

    chocolate question for TA

--- frontend/scripts/home.js
+++ frontend/scripts/home.js
@@ -14,3 +14,5 @@ async function loadHomeContent() {
 document.addEventListener("DOMContentLoaded", () => {
   loadHomeContent();
 });
+
+//Chocolate question for TA's, find the welcome message on Home page.
